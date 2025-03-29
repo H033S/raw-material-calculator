f522c5cabcf83cf5c237fbf5e48e2e6e61718c52
H033S <tonito.nazco@gmail.com>
2025-03-29 18:45:59 -0400

    Initial API Project

---
+++ api/src/main/java/edu/fiu/hackathon/material_calculator/MaterialCalculatorApplication.java
@@ -0,0 +1,13 @@
+package edu.fiu.hackathon.material_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialCalculatorApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/edu/fiu/hackathon/material_calculator/MaterialCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fiu.hackathon.material_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
