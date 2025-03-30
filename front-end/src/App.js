a09c55ac55f72f427125394505a3a07e1d8251c7
H033S <tonito.nazco@gmail.com>
2025-03-30 17:48:15 -0400

    Started isolation of the app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,33 +1,13 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import index from './index.css';
-import MultiRecipeCalculator from './MultiRecipeCalculator';
-
-
+import "./App.css";
+import React from "react"
+import { ReceiptCalculator } from "./components/calculator/ReceiptCalculator";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <h1>Multi Recipe Calculator</h1> */}
-      <MultiRecipeCalculator />
+      <ReceiptCalculator />
     </div>
   );
 }
 
 export default App;
-
