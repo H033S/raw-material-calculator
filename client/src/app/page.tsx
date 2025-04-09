a24d17b3e02fe38a8123d88fb1ca8c6416105ebd
H033S <tonito.nazco@gmail.com>
2025-04-09 07:23:48 -0400

    upd: Extract Navbar in a Separated Component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,55 +1,11 @@
 "use client";
 
-import { useEffect } from 'react';
-import UserProfileComponent from './components/UserProfileComponent';
+import Navbar from './components/Navbar';
 
 export default function Home() {
-  useEffect(() => {
-    require('bootstrap/dist/js/bootstrap.bundle.min.js');
-  }, []);
-
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand me-2" href="#">Navbar</a>
-          <div className="d-flex flex-grow-1 align-items-center">
-            <button 
-              className="navbar-toggler border-0 me-3" 
-              type="button" 
-              data-bs-toggle="collapse" 
-              data-bs-target="#navbarNavDropdown" 
-              aria-controls="navbarNavDropdown" 
-              aria-expanded="false" 
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Products
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Raw Materials
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="ms-auto">
-              <UserProfileComponent />
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
     </div>
   );
 }
