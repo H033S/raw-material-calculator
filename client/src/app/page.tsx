054f8bc8f2cbc08c1fda3614ecc3dfbdc92183fc
H033S <tonito.nazco@gmail.com>
2025-04-09 07:21:15 -0400

    upd: Rearrange navbar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import UserProfileComponent from './components/UserProfileComponent';
+
 export default function Home() {
   useEffect(() => {
     require('bootstrap/dist/js/bootstrap.bundle.min.js');
@@ -11,41 +12,42 @@ export default function Home() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            {" "}
-            Navbar{" "}
-          </a>
-          <button 
-            className="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNavDropdown" 
-            aria-controls="navbarNavDropdown" 
-            aria-expanded="false" 
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Products
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Raw Materials
-                </a>
-              </li>
-            </ul>
+          <a className="navbar-brand me-2" href="#">Navbar</a>
+          <div className="d-flex flex-grow-1 align-items-center">
+            <button 
+              className="navbar-toggler border-0 me-3" 
+              type="button" 
+              data-bs-toggle="collapse" 
+              data-bs-target="#navbarNavDropdown" 
+              aria-controls="navbarNavDropdown" 
+              aria-expanded="false" 
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Products
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Raw Materials
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="ms-auto">
+              <UserProfileComponent />
+            </div>
           </div>
-          <UserProfileComponent />
         </div>
       </nav>
     </div>
