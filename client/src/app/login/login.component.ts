cd3e7a5073cada2b642af6c42c580109fe201c2f
H033S <tonito.nazco@gmail.com>
2025-05-01 07:02:01 -0400

    Added validation in the Login Form

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface LoginCredentials {
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  credentials: LoginCredentials = {
+    username: '',
+    password: '',
+  };
+  submitted = false;
 
+  onSubmit() {
+    this.submitted = true;
+  }
 }
