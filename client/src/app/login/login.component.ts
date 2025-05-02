7b93d879ea8dab62e6aaf5caf70c540ae4b2a18e
H033S <tonito.nazco@gmail.com>
2025-05-02 07:10:37 -0400

    Revamped Login Form. Included Font Awesome

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 interface LoginCredentials {
-  username: string;
+  email: string;
   password: string;
 }
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, FontAwesomeModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  faFacebook = faFacebook;
+  faGoogle = faGoogle;
   credentials: LoginCredentials = {
-    username: '',
+    email: '',
     password: '',
   };
   submitted = false;
