56c38dbfd1a7d4684f3c422d6e5a16aa29310c97
H033S <tonito.nazco@gmail.com>
2025-05-14 23:06:32 -0400

    feat: added routes for components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import {LoginComponent} from './login/login.component';
+import {AppComponent} from './app.component';
+import {ForgotPasswordCodeComponent} from './forgot-password-code/forgot-password-code.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: "", component: AppComponent},
+  {path: "auth/forgot-password", component: ForgotPasswordComponent},
+  {path: "auth/forgot-password-code", component: ForgotPasswordCodeComponent},
+  {path: "auth/login", component: LoginComponent},
+];
